9982ba8b1b705d519125c8135b7b5b9fbaa4b2a9
Oscar Rovira <oscar.rovira@inetum.com>
2023-05-17 12:17:32 +0200

    Refactor switch

--- DiscountManager.cs
+++ DiscountManager.cs
@@ -5,24 +5,26 @@ public class DiscountManager
         decimal priceAfterDiscount = 0;
         decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > 5) ?
                 (decimal)5 / 100 : (decimal)timeOfHavingAccountInYears / 100;
-        if (accountStatus == AccountStatus.NotRegistered)
+        switch (accountStatus)
         {
-            priceAfterDiscount = price;
-        }
-        else if (accountStatus == AccountStatus.SimpleCustomer)
-        {
-            priceAfterDiscount = (price - (0.1m * price)) -
-                 (discountForLoyaltyInPercentage * (price - (0.1m * price)));
-        }
-        else if (accountStatus == AccountStatus.ValuableCustomer)
-        {
-            priceAfterDiscount = (0.7m * price) -
-                 (discountForLoyaltyInPercentage * (0.7m * price));
-        }
-        else if (accountStatus == AccountStatus.MostValuableCustomer)
-        {
-            priceAfterDiscount = (price - (0.5m * price)) -
-                 (discountForLoyaltyInPercentage * (price - (0.5m * price)));
+            case AccountStatus.NotRegistered:
+                priceAfterDiscount = price;
+                break;
+            case AccountStatus.SimpleCustomer:
+                priceAfterDiscount = (price - (0.1m * price));
+                priceAfterDiscount = priceAfterDiscount -
+                                     (discountForLoyaltyInPercentage * priceAfterDiscount);
+                break;
+            case AccountStatus.ValuableCustomer:
+                priceAfterDiscount = (0.7m * price);
+                priceAfterDiscount = priceAfterDiscount -
+                                     (discountForLoyaltyInPercentage * priceAfterDiscount);
+                break;
+            case AccountStatus.MostValuableCustomer:
+                priceAfterDiscount = (price - (0.5m * price));
+                priceAfterDiscount = priceAfterDiscount -
+                                     (discountForLoyaltyInPercentage * priceAfterDiscount);
+                break;
         }
 
         return priceAfterDiscount;
