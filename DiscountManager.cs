cb759c2419ede65932b5fee884c20679df225190
Oscar Rovira <oscar.rovira@inetum.com>
2023-05-17 12:23:11 +0200

    Refactor extension methods

--- DiscountManager.cs
+++ DiscountManager.cs
@@ -3,27 +3,25 @@ public class DiscountManager
     public decimal ApplyDiscount(decimal price, AccountStatus accountStatus, int timeOfHavingAccountInYears)
     {
         decimal priceAfterDiscount = 0;
-        decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY) ?
-                (decimal)Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY / 100 : (decimal)timeOfHavingAccountInYears / 100;
         switch (accountStatus)
         {
             case AccountStatus.NotRegistered:
                 priceAfterDiscount = price;
                 break;
             case AccountStatus.SimpleCustomer:
-                priceAfterDiscount = (price - (Constants.DISCOUNT_FOR_SIMPLE_CUSTOMERS * price));
-                priceAfterDiscount = priceAfterDiscount -
-                                     (discountForLoyaltyInPercentage * priceAfterDiscount);
+                priceAfterDiscount = price
+                                        .ApplyDiscountForAccountStatus(Constants.DISCOUNT_FOR_SIMPLE_CUSTOMERS)
+                                        .ApplyDiscountForTimeOfHavingAccount(timeOfHavingAccountInYears);
                 break;
             case AccountStatus.ValuableCustomer:
-                priceAfterDiscount = (price - (Constants.DISCOUNT_FOR_VALUABLE_CUSTOMERS * price));
-                priceAfterDiscount = priceAfterDiscount -
-                                     (discountForLoyaltyInPercentage * priceAfterDiscount);
+                priceAfterDiscount = price
+                                        .ApplyDiscountForAccountStatus(Constants.DISCOUNT_FOR_VALUABLE_CUSTOMERS)
+                                        .ApplyDiscountForTimeOfHavingAccount(timeOfHavingAccountInYears);
                 break;
             case AccountStatus.MostValuableCustomer:
-                priceAfterDiscount = (price - (Constants.DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS * price));
-                priceAfterDiscount = priceAfterDiscount -
-                                     (discountForLoyaltyInPercentage * priceAfterDiscount);
+                priceAfterDiscount = price
+                                        .ApplyDiscountForAccountStatus(Constants.DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS)
+                                        .ApplyDiscountForTimeOfHavingAccount(timeOfHavingAccountInYears);
                 break;
             default:
                 throw new NotImplementedException();
---
+++ PriceExtensions.cs
@@ -0,0 +1,18 @@
+public static class PriceExtensions
+{
+    public static decimal ApplyDiscountForAccountStatus
+                  (this decimal price, decimal discountSize)
+    {
+        return price - (discountSize * price);
+    }
+
+    public static decimal ApplyDiscountForTimeOfHavingAccount
+                  (this decimal price, int timeOfHavingAccountInYears)
+    {
+        decimal discountForLoyaltyInPercentage =
+                (timeOfHavingAccountInYears > Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY) ?
+                (decimal)Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY / 100 :
+                (decimal)timeOfHavingAccountInYears / 100;
+        return price - (discountForLoyaltyInPercentage * price);
+    }
+}
