ef182e4b4cf05cfe9d848a1368f0fd6fb702a276
Oscar Rovira <oscar.rovira@inetum.com>
2023-05-17 12:16:50 +0200

    Refactor to enum

---
+++ AccountStatus.cs
@@ -0,0 +1,7 @@
+public enum AccountStatus
+{
+  NotRegistered = 1,
+  SimpleCustomer = 2,
+  ValuableCustomer = 3,
+  MostValuableCustomer = 4
+}
--- DiscountManager.cs
+++ DiscountManager.cs
@@ -1,25 +1,25 @@
 public class DiscountManager
 {
-    public decimal ApplyDiscount(decimal price, int accountStatus, int timeOfHavingAccountInYears)
+    public decimal ApplyDiscount(decimal price, AccountStatus accountStatus, int timeOfHavingAccountInYears)
     {
         decimal priceAfterDiscount = 0;
         decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > 5) ?
                 (decimal)5 / 100 : (decimal)timeOfHavingAccountInYears / 100;
-        if (accountStatus == 1)
+        if (accountStatus == AccountStatus.NotRegistered)
         {
             priceAfterDiscount = price;
         }
-        else if (accountStatus == 2)
+        else if (accountStatus == AccountStatus.SimpleCustomer)
         {
             priceAfterDiscount = (price - (0.1m * price)) -
                  (discountForLoyaltyInPercentage * (price - (0.1m * price)));
         }
-        else if (accountStatus == 3)
+        else if (accountStatus == AccountStatus.ValuableCustomer)
         {
             priceAfterDiscount = (0.7m * price) -
                  (discountForLoyaltyInPercentage * (0.7m * price));
         }
-        else if (accountStatus == 4)
+        else if (accountStatus == AccountStatus.MostValuableCustomer)
         {
             priceAfterDiscount = (price - (0.5m * price)) -
                  (discountForLoyaltyInPercentage * (price - (0.5m * price)));
